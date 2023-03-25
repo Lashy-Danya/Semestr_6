d5fa3bfb2df50bf01e40a37292f7b6c5e725d9c6
Danya Alekseev <lashywork@gmail.com>
2023-03-25 23:49:53 +0300

    end lab 1

--- mikhailov/lab_1/a.py
+++ mikhailov/lab_1/a.py
@@ -2,70 +2,89 @@ import numpy as np
 from queue import PriorityQueue
 from graphviz import Digraph
 
-def a_star(initial_state, goal_state):
-    frontier = PriorityQueue()
-    frontier.put((0, initial_state))
-    came_from = {str(initial_state): None}
-    cost_so_far = {str(initial_state): 0}
-
-    while not frontier.empty():
-        current_state = frontier.get()[1]
-
-        if np.array_equal(current_state, goal_state):
-            break
-
-        for next_state in get_neighbors(current_state):
-            new_cost = cost_so_far[str(current_state)] + 1
-            if str(next_state) not in cost_so_far or new_cost < cost_so_far[str(next_state)]:
-                cost_so_far[str(next_state)] = new_cost
-                priority = new_cost + manhattan_distance(next_state, goal_state)
-                frontier.put((priority, next_state))
-                came_from[str(next_state)] = current_state
-
-    return came_from, cost_so_far
-
-def get_neighbors(state):
-    neighbors = []
-    zero_position = np.where(state == 0)
-
-    for move in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-        neighbor_position = zero_position[0] + move[0], zero_position[1] + move[1]
-
-        if 0 <= neighbor_position[0] < 3 and 0 <= neighbor_position[1] < 3:
-            neighbor_state = state.copy()
-            neighbor_state[zero_position], neighbor_state[neighbor_position] = \
-                neighbor_state[neighbor_position], neighbor_state[zero_position]
-            neighbors.append(neighbor_state)
-
-    return neighbors
-
-def manhattan_distance(state, goal_state):
-    distance = 0
-
-    for i in range(3):
-        for j in range(3):
-            if state[i][j] == 0:
-                continue
-            goal_position = np.where(goal_state == state[i][j])
-            distance += abs(i - goal_position[0]) + abs(j - goal_position[1])
-
-    return distance
-
-def create_graph(came_from):
-    graph = Digraph()
-
-    for node in came_from.keys():
-        graph.node(node)
-
-    for node, parent in came_from.items():
-        if parent is not None:
-            graph.edge(str(parent), node)
-
-    return graph
-
-initial_state = np.array([[2, 8, 3], [1, 6, 4], [7, 0, 5]])
+# Целевое состояние игры
 goal_state = np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]])
 
-came_from, cost_so_far = a_star(initial_state, goal_state)
-graph = create_graph(came_from)
-graph.render('game_tree', view=True)
+# Определяем эвристическую функцию (расстояние Манхэттена)
+def heuristic(current_state, goal_state):
+    # Вычисляем расстояние Манхэттена между текущим состоянием и состоянием целевым
+    return np.sum(np.abs(current_state - goal_state))
+
+# функция для проверки, является ли состояние целевым
+def is_goal(state):
+    return np.array_equal(state, goal_state)
+
+# Функция для генерации дочерних узлов
+def find_moves(state):
+    moves = []
+    zero_pos = np.where(state == 0)
+    zero_row, zero_col = zero_pos[0][0], zero_pos[1][0]
+    for row, col in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+        new_row, new_col = zero_row + row, zero_col + col
+        if (new_row >= 0) and (new_row <= 2) and (new_col >= 0) and (new_col <= 2):
+            new_state = state.copy()
+            new_state[zero_row, zero_col], new_state[new_row, new_col] = \
+            new_state[new_row, new_col], new_state[zero_row, zero_col]
+            moves.append(new_state)
+    return moves
+
+# Определяем функцию поиска A*
+def a_star_search(start_state):
+
+    graph = Digraph()
+    graph.node(str(start_state), str(start_state))
+    steps = 0
+
+    queue = PriorityQueue()
+    queue.put((0, [start_state.tolist()]))
+
+    visited = []
+
+    while not queue.empty():
+        path_cost, path = queue.get()
+
+        current_state = path[-1]
+        current_state = np.array(current_state)
+
+        if is_goal(current_state):
+
+            print(f'Steps: {steps}')
+
+            # for i, state in enumerate(path):
+            #     graph.node(str(i), str(np.array(state)))
+
+            # for i in range(len(path) - 1):
+            #     graph.edge(str(i), str(i+1))
+
+            return path, graph
+        
+        steps += 1
+        
+        visited.append(str(current_state))
+
+        for move in find_moves(current_state):
+            if str(move) not in visited:
+                graph.node(str(move), str(move))
+                graph.edge(str(current_state), str(move))
+
+                new_path_cost = path_cost + 1
+                new_heuristic = heuristic(move, goal_state)
+
+                total_cost = new_path_cost + new_heuristic
+
+                new_path = path + [move.tolist()]
+                queue.put((total_cost, new_path))
+
+    return None
+
+if __name__ == '__main__':
+    # Исходное состояние игры
+    start_state = np.array([[2, 8, 3], [1, 6, 4], [7, 0, 5]])
+    
+    path, graph = a_star_search(start_state)
+    # print(path)
+    if path is not None:
+        graph.render('a*')
+        graph.view()
+    else:
+        print('Решение не найдено')
--- mikhailov/lab_1/brute_force_method.py
+++ mikhailov/lab_1/brute_force_method.py
@@ -1,75 +1,11 @@
-# import numpy as np
-# from queue import Queue
-# from graphviz import Digraph
-
-# dot = Digraph('Graph', filename='brute_force.gv')
-
-# # Функция для получения возможных следующих состояний игры
-# def get_next_states(state):
-#     zero_index = np.where(state == 0)
-#     zero_row, zero_col = zero_index[0][0], zero_index[1][0]
-#     next_states = []
-#     for row, col in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
-#         new_row, new_col = zero_row + row, zero_col + col
-#         if (new_row >= 0) and (new_row <= 2) and (new_col >= 0) and (new_col <= 2):
-#             new_state = state.copy()
-#             new_state[zero_row, zero_col], new_state[new_row, new_col] = new_state[new_row, new_col], new_state[zero_row, zero_col]
-#             next_states.append(new_state)
-#     return next_states
-
-# # Функция для проверки, является ли состояние конечным
-# def is_goal(state):
-#     return np.array_equal(state, np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]]))
-
-# # Начальное состояние игры
-# initial_state = np.array([[2, 8, 3], [1, 6, 4], [7, 0, 5]])
-# dot.node(str(initial_state))
-# # Создаем очередь и добавляем начальное состояние
-# q = Queue()
-# q.put(initial_state)
-# steps = 0
-
-# # Проходимся по всем возможным состояниям, пока не найдем конечное
-# while not q.empty():
-#     curr_state = q.get()
-#     steps += 1
-#     if is_goal(curr_state):
-#         print("Решение найдено!")
-#         break
-#     next_states = get_next_states(curr_state)
-#     for state in next_states:
-#         q.put(state)
-# print(steps)
-# print(curr_state)
-
-# # for item in range(q.qsize()):
-# #     print(q.get())
-
-# dot.view()
-
 import numpy as np
 from queue import Queue
 from graphviz import Digraph
 
-# исходное состояние игры
-start_state = np.array([[2, 8, 3],[1, 6, 4],[7, 0, 5]])
-
 # конечное состояние игры
 goal_state = np.array([[1, 2, 3],[8, 0, 4],[7, 6, 5]])
 
-# функция для нахождения всех возможных ходов
-# def find_moves(state):
-#     moves = []
-#     zero_pos = np.argwhere(state == 0)[0]
-#     for pos in [(0,1), (1,0), (0,-1), (-1,0)]:
-#         move_pos = zero_pos + pos
-#         if (move_pos >= 0).all() and (move_pos < 3).all():
-#             new_state = state.copy()
-#             new_state[zero_pos[0], zero_pos[1]] = new_state[move_pos[0], move_pos[1]]
-#             new_state[move_pos[0], move_pos[1]] = 0
-#             moves.append(new_state)
-#     return moves
-
+# Функция для генерации дочерних узлов
 def find_moves(state):
     moves = []
     zero_pos = np.where(state == 0)
@@ -78,7 +14,8 @@ def find_moves(state):
         new_row, new_col = zero_row + row, zero_col + col
         if (new_row >= 0) and (new_row <= 2) and (new_col >= 0) and (new_col <= 2):
             new_state = state.copy()
-            new_state[zero_row, zero_col], new_state[new_row, new_col] = new_state[new_row, new_col], new_state[zero_row, zero_col]
+            new_state[zero_row, zero_col], new_state[new_row, new_col] = \
+            new_state[new_row, new_col], new_state[zero_row, zero_col]
             moves.append(new_state)
     return moves
 
@@ -89,23 +26,34 @@ def is_goal(state):
 # функция для построения графа перебора
 def build_graph(start_state):
     graph = Digraph()
+    steps = 0
     graph.node(str(start_state))
     queue = Queue()
     queue.put(start_state)
-    visited = set([str(start_state)])
+    visited = [str(start_state)]
     while not queue.empty():
         state = queue.get()
         if is_goal(state):
+            print(f'Steps: {steps}')
             return graph
+        steps += 1
         for move in find_moves(state):
             if str(move) not in visited:
-                visited.add(str(move))
+                visited.append(str(move))
                 graph.node(str(move), str(move))
                 graph.edge(str(state), str(move))
                 queue.put(move)
 
-# построение графа перебора
-graph = build_graph(start_state)
-graph.render('game_tree')
+    return None
 
-# реалезуй метод полного перебора для игры в восемь, результат должен быть [[1,2,3],[8,0,4],[7,6,5]] используя python и библиотеки numpy, queue и graphviz
+if __name__ == '__main__':
+    # Исходное состояние игры
+    start_state = np.array([[2, 8, 3], [1, 6, 4], [7, 0, 5]])
+
+    # построение графа перебора
+    graph = build_graph(start_state)
+    if graph is not None:
+        graph.render('game_tree')
+        graph.view()
+    else:
+        print(f'Значение графа: {graph}')
