86984f2483d9588323c089b41658df1ed15271da
Danya Alekseev <lashywork@gmail.com>
2023-05-09 21:45:12 +0300

    add

--- cryptography/lab_5/des.py
+++ cryptography/lab_5/des.py
@@ -366,10 +366,79 @@ if __name__ == ('__main__'):
     # deshifr = des.crypt(shifr, DECRYPT)
 
     # Режим OFB, где k = 8 bit
-    des = DES(key, OFB, iv)
-    shifr = des.crypt(test, ENCRYPT)
-    deshifr = des.crypt(shifr, DECRYPT)
+    # des = DES(key, OFB, iv)
+    # shifr = des.crypt(test, ENCRYPT)
+    # deshifr = des.crypt(shifr, DECRYPT)
 
-    print(f'Начальный текст: {test}')
-    print(f'Зашифрованый текст: {shifr}')
-    print(f'Расшифрованый текст: {deshifr}')
+    # print(f'Начальный текст: {test}')
+    # print(f'Зашифрованый текст: {shifr}')
+    # print(f'Расшифрованый текст: {deshifr}')
+
+    import os
+    import codecs
+    import matplotlib.pyplot as plt
+    from collections import Counter
+
+    cp866_chars = []
+    for i in range(256):
+        try:
+            char = codecs.decode(bytes([i]), 'cp866')
+            cp866_chars.append(char)
+        except UnicodeDecodeError:
+            pass
+
+     # Получение пути к запускаемому файлу
+    current_file_path = os.path.abspath(__file__)
+    current_path = os.path.dirname(current_file_path)
+
+    # Получение названия файла и путь к нему в папке с запускаемым скриптом
+    file_name = input('Введите название файла формата txt: ')
+    file_path = os.path.join(current_path, file_name)
+
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            text = f.read()
+
+        key = 'ключ64б!'
+        iv = 'тестив01'
+
+        des = DES(key, OFB, iv)
+
+        shifr = des.crypt(text, ENCRYPT)
+        deshifr = des.crypt(shifr, DECRYPT)
+
+        print(f'Начальный текст: {text}')
+        print(f'Зашифрованый текст: {shifr}')
+        print(f'Расшифрованый текст: {deshifr}')
+
+        # Получаем частотный словарь символов
+        freq_dict = Counter(shifr)
+
+        freq_dict_text = Counter(text)
+
+        fig1, (ax, ax1) = plt.subplots(2, 1, figsize=(16, 6))
+
+        ax.bar([i for i in cp866_chars[:128]], [freq_dict_text.get(i, 0) for i in cp866_chars[:128]])
+        ax.set_title('Гистограмма начального текста')
+        ax.set_xlabel('Символы')
+        ax.set_ylabel('Частота')
+
+        ax1.bar([i for i in cp866_chars[128:]], [freq_dict_text.get(i, 0) for i in cp866_chars[128:]])
+        ax1.set_xlabel('Символы')
+        ax1.set_ylabel('Частота')
+
+        fig2, (ax2, ax3) = plt.subplots(2, 1, figsize=(16, 6))
+
+        ax2.bar([i for i in cp866_chars[:128]], [freq_dict.get(i, 0) for i in cp866_chars[:128]])
+        ax2.set_title('Гистограмма зашифрованного текста')
+        ax2.set_xlabel('Символы')
+        ax2.set_ylabel('Частота')
+
+        ax3.bar([i for i in cp866_chars[128:]], [freq_dict.get(i, 0) for i in cp866_chars[128:]])
+        ax3.set_xlabel('Символы')
+        ax3.set_ylabel('Частота')
+
+        plt.show()
+
+    else:
+        print('Такого файла нет')
